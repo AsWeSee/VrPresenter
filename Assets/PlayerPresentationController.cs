c68318db365a85cfe829969a73636a4b2204e7bb
AsWeSee <smolev.vitaliy@gmail.com>
2019-09-13 03:50:09 +0300

    added furniture

---
+++ Assets/PlayerPresentationController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using OVR.OpenVR;
+using UnityEngine;
+
+public class PlayerPresentationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Three))
+        {
+            
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Four))
+        {
+            
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Left))
+        {
+            
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Right))
+        {
+            
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Up))
+        {
+            
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Down))
+        {
+            
+        }
+        if (OVRInput.GetDown(OVRInput.Button.DpadDown))
+        {
+            
+        }
+    }
+}
