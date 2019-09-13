28ee3c73b94c1017f3d6291fb6c81d495ceff864
AsWeSee <smolev.vitaliy@gmail.com>
2019-09-13 05:57:59 +0300

    added screen object, presentation slides, and controller input

--- Assets/PlayerPresentationController.cs
+++ Assets/PlayerPresentationController.cs
@@ -2,51 +2,74 @@
 using System.Collections.Generic;
 using OVR.OpenVR;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerPresentationController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Sprite[] sprites;
+    public Image screen;
+    public SpriteRenderer screen2;
+    public int currentSlide = 0;
+    
     void Start()
     {
+        loadSlides();
+    }
+
+    void loadSlides()
+    {
+        currentSlide = 0;
+        //TODO: брать все файлы из конкретной папки
+        sprites = new Sprite[5];
+        //TODO: рассмотреть возможность загрузки pdf
+        sprites[0] = Resources.Load<Sprite>("Presentation/1/diff");
+        sprites[1] = Resources.Load<Sprite>("Presentation/1/1");
+        sprites[2] = Resources.Load<Sprite>("Presentation/1/2");
+        sprites[3] = Resources.Load<Sprite>("Presentation/1/3");
+        sprites[4] = Resources.Load<Sprite>("Presentation/1/4");
+        updateSlide();
+    }
+
+    void updateSlide()
+    {
+        //Если пошли дальше размера презентации, то остаемся в конце
+        if (currentSlide >= sprites.Length)
+            currentSlide = sprites.Length - 1;
+//            currentSlide = 0; //Как альтернатива, вернуться на первый слайд
         
+        //Если мы попытались пойти назад на первом слайде, то слайд не меняется.
+        if (currentSlide < 0)
+            currentSlide = 0;
+//            currentSlide = sprites.Length - 1; //либо загружаем последний слайд
+        
+        
+        screen.sprite = sprites[currentSlide];
+        screen2.sprite = sprites[currentSlide];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.One))
+        //Переключение слайдов вперёд и назад
+        if (OVRInput.GetDown(OVRInput.Button.Four))
         {
-            
-        }
-        if (OVRInput.GetDown(OVRInput.Button.Two))
-        {
-            
+            currentSlide += 1;
+            updateSlide();
         }
         if (OVRInput.GetDown(OVRInput.Button.Three))
         {
-            
+            currentSlide -= 1;
+            updateSlide();
         }
-        if (OVRInput.GetDown(OVRInput.Button.Four))
+        
+        //Возврат к началу презентации и обновление слайдов из папки
+        if (OVRInput.GetDown(OVRInput.Button.Two))
         {
-            
+            loadSlides();
         }
-        if (OVRInput.GetDown(OVRInput.Button.Left))
-        {
-            
-        }
-        if (OVRInput.GetDown(OVRInput.Button.Right))
-        {
-            
-        }
-        if (OVRInput.GetDown(OVRInput.Button.Up))
-        {
-            
-        }
-        if (OVRInput.GetDown(OVRInput.Button.Down))
-        {
-            
-        }
-        if (OVRInput.GetDown(OVRInput.Button.DpadDown))
+        
+        //Проигрывание аплодисментов
+        if (OVRInput.GetDown(OVRInput.Button.One))
         {
             
         }
